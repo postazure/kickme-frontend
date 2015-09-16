9fa5c6875f01f8c0db105f585c01ed331dcf7662
Max Gordon <gordonmaxc@gmail.com>
2015-09-15 22:00:12 -0700

    first commit

---
+++ run.sh
@@ -0,0 +1,4 @@
+# Simple Python Server to run
+# $ bash run.sh
+
+python -m SimpleHTTPServer
---
+++ watch_transformer.sh
@@ -0,0 +1,4 @@
+# Watches src/ and translates JSX
+# to run $ bash watch_transformer.sh
+
+jsx --watch src/ build/
