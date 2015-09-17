2f0300d1ec7ae6d31f731b3ba5db22db82288c8c
Max Gordon <gordonmaxc@gmail.com>
2015-09-16 22:20:32 -0700

    Added watch list

--- src/views/watch_list.js
+++ src/views/watch_list.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import AddNewCreators from './watch_list/addNewCreators.js'
 
 export default class WatchList extends React.Component {
+    getProjectCreators() {
+        // get project creators from api
+        return [
+            { name: 'CoolMiniOrNot', bio: 'bar bar bar foo', avatar: 'https://ksr-ugc.imgix.net/avatars/2326978/cmonlogo250.original.jpg?v=1393237255&w=80&h=80&fit=crop&auto=format&q=92&s=0c4874ba952f7b79025eb92d297c85d9' }
+        ]
+    }
+
+    renderProjectCreators() {
+        let creatorsList = this.getProjectCreators().map(function(creator) {
+            return(
+                <div className="item">
+                    <img className="ui avatar image" src={creator.avatar} />
+                    <div className="content">
+                        <a className="header">{creator.name}</a>
+                        <div className="description">{creator.bio}</div>
+                    </div>
+                </div>
+            )
+        });
+        return(
+            <div className="ui list">
+                {creatorsList}
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div className=" ui raised very padded text container segment">
-                Watch List
+            <div className="ui raised very padded container segment">
+                <AddNewCreators />
+                <h3 className="ui header">
+                    You're Watching
+                </h3>
+
+                {this.renderProjectCreators()}
             </div>
         )
     }
---
+++ src/views/watch_list/addNewCreators.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class AddNewCreators extends React.Component {
+    render() {
+        return (
+            <div className="ui input">
+                <input type="text" placeholder="Search for Project Creator" />
+            </div>
+        )
+    }
+}
+
